c61d66e5105022ccc0ecdc19c5928c232ffe4640
白色の物语 <42845041+Whitestorys@users.noreply.github.com>
2021-09-27 01:34:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,12 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' ./feeds/luci/collections/luci/Makefile
 # Modify default PASSWORD
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$BtNu28UV$VAZEz4CDe1k7Dvar7Ftji0/g' ./package/lean/default-settings/files/zzz-default-settings
+
+# 1.Set the download repository of the OpenWrt files to your github.com
+# sed -i "s|https.*/OpenWrt|https://github.com/Aibx/OpenWRT-S905D|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 2.Set the keywords of Tags in your github.com Releases
+# sed -i "s|ARMv8|RELEASES_TAGS_KEYWORD|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 3.Set the suffix of the OPENWRT files in your github.com Releases
+# sed -i "s|.img.gz|.OPENWRT_SUFFIX|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 4.Set the download path of the kernel in your github.com repository
+# sed -i "s|opt/kernel|https://github.com/Aibx/OpenWRT-S905D/opt/kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
